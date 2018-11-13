f3b0772f3694bffdb4d9920058389c7a55b1e5fe
Christopher Soltis <christopher@Christophers-MacBook-Pro.local>
2018-11-13 14:04:36 -0500

    Data Types

--- Python101.py
+++ Python101.py
@@ -10,3 +10,14 @@
 
 # Data Types  
 # - string - English stuff.  
+month = "November"
+print type(month)
+date = 13;
+print type(date)
+dateAsFloat = 13.0
+print type(dateAsFloat);
+aList = []
+print type(aList)
+aDictionary = {};
+print(aDictionary)
+
