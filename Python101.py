e9ff6e4feffc59736a461581b62bba70e771e76a
cdsalty <soltiscd@gmail.com>
2018-11-13 14:28:19 -0500

    Add Operators

--- Python101.py
+++ Python101.py
@@ -1,23 +1,33 @@
-# print "Hello, World";
-# print """This 
-# will print on 
-# several lines""";
-# print "Christopher 'd' Soltis";
-# #this is easy an way to comment.
-# # or use command and the backslash. /
-# theBestClass = "The 11-18 immersive";
-# print theBestClass;
+# # print "Hello, World";
+# # print """This 
+# # will print on 
+# # several lines""";
+# # print "Christopher 'd' Soltis";
+# # #this is easy an way to comment.
+# # # or use command and the backslash. /
+# # theBestClass = "The 11-18 immersive";
+# # print theBestClass;
 
-# Data Types  
-# - string - English stuff.  
-month = "November"
-print type(month)
-date = 13;
-print type(date)
-dateAsFloat = 13.0
-print type(dateAsFloat);
-aList = []
-print type(aList)
-aDictionary = {};
-print(aDictionary)
+# # Data Types  
+# # - string - English stuff.  
+# month = "November"
+# print type(month)
+# date = 13;
+# print type(date)
+# dateAsFloat = 13.0
+# print type(dateAsFloat);
+# aList = []
+# print type(aList)
+# aDictionary = {};
+# print(aDictionary)
 
+# first = "Christopher";
+# last = "Soltis";
+# fullName = first + " " + last;
+# print fullName;
+
+# # cast = change a variable to a new date type
+# fourteen = int("10") + 4
+# print (fourteen);
+
+print "-" * 20
