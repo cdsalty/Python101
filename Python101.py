c2bba5fa949dca7561085c6e95d14ddb0ffea5cf
Christopher Soltis <christopher@Christophers-MacBook-Pro.local>
2018-11-13 12:22:14 -0500

    My first commit

---
+++ Python101.py
@@ -0,0 +1,12 @@
+# print "Hello, World";
+# print """This 
+# will print on 
+# several lines""";
+# print "Christopher 'd' Soltis";
+# #this is easy an way to comment.
+# # or use command and the backslash. /
+# theBestClass = "The 11-18 immersive";
+# print theBestClass;
+
+# Data Types  
+# - string - English stuff.  
